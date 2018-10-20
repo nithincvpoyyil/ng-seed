c339eb90407c33d459da1e6a455ba624f63eb128
nithin <nithincv1@gmail.com>
2018-10-20 16:16:43 +1100

    app routing

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent} from '../dashboard/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: LandingPageComponent
+  },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes, { useHash: true, enableTracing: true })
+  ],
+  exports: [RouterModule],
+  declarations: []
 })
-export class AppRoutingRoutingModule { }
+export class RoutingModule {}
